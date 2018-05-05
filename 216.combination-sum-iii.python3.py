94088afb08e8020759a8714394a7830cc3d7f5d9
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-05-05 13:55:53 -0700

    update 216.combination-sum-iii.python3.py

--- 216.combination-sum-iii.python3.py
+++ 216.combination-sum-iii.python3.py
@@ -38,34 +38,23 @@
 #
 #
 class Solution:
-    def helper(self, k, n, start=1, seen=set()):
-        if start>9: return None
-
-        if k==0:
-            if n==0: return []
-            else: return None
-        if k==1:
-            if n>=start and n<10:  return [[n]]
-            else: return None
-        res = []
-
-        for i in range(start, 10):
-            if i>n: break
-            if i in seen: continue
-            seen.add(i)
-            subress = self.helper(k-1, n-i, i+1, seen)
-            seen.remove(i)
-            if subress is None or len(subress) == 0:
-                continue
-            for subres in subress:
-                t = [i] + subres
-                res.append(t)
-        return res
-
-    def combinationSum3(self, k, n):
+    def combinationSum3(self, k, n, start = 1):
         """
         :type k: int
         :type n: int
         :rtype: List[List[int]]
         """
-        return self.helper(k, n)
+        if n == start and k==1:
+            ans = [[start]]
+        elif n < start or k<=0:
+            ans = []
+        else:
+            ans = []
+            sr1 = self.combinationSum3(k-1, n-start, start+1) if start < 9 else []
+            for s in sr1:
+                # if len(s) > 0:
+                ans.append([start] + s)
+            sr2 = self.combinationSum3(k, n, start+1) if start < 9 else []
+            ans += sr2
+        # print("k={},n={},start={},ans={}".format(k,n,start, ans))
+        return ans
