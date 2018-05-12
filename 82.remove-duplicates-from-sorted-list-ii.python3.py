4dca43512d40d03322ea690f5d52776b31274827
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-05-12 16:28:29 -0700

    update 82.remove-duplicates-from-sorted-list-ii.python3.py

---
+++ 82.remove-duplicates-from-sorted-list-ii.python3.py
@@ -0,0 +1,57 @@
+#
+# [82] Remove Duplicates from Sorted List II
+#
+# https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/description/
+#
+# algorithms
+# Medium (30.09%)
+# Total Accepted:    135.4K
+# Total Submissions: 449.8K
+# Testcase Example:  '[1,2,3,3,4,4,5]'
+#
+# Given a sorted linked list, delete all nodes that have duplicate numbers,
+# leaving only distinct numbers from the original list.
+#
+# Example 1:
+#
+#
+# Input: 1->2->3->3->4->4->5
+# Output: 1->2->5
+#
+#
+# Example 2:
+#
+#
+# Input: 1->1->1->2->3
+# Output: 2->3
+#
+#
+#
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+
+        sentinel = ListNode(None)
+        sentinel.next = head
+
+        en = sentinel
+        h = head
+        while h and h.next:
+            if h.val != h.next.val:
+                en = h
+            else:
+                while h.next and h.val == h.next.val:
+                    h=h.next
+                en.next = h.next
+            h = h.next
+
+        return sentinel.next
