37080d5fc3d55589691e51dbe4f17de027f03ec2
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-05-13 11:33:38 -0700

    update 89.gray-code.python3.py

--- 89.gray-code.python3.py
+++ 89.gray-code.python3.py
@@ -42,18 +42,42 @@ class Solution:
         :type n: int
         :rtype: List[int]
         """
-        if n == 0:
-            return [0]
+        # Very clever solution
+        # https://leetcode.com/problems/gray-code/discuss/29893/One-liner-Python-solution-(with-demo-in-comments)
+        # Generates further patterns by observing past numbers
+        #   0      base
+        #   --
+        #   1      2**0 + 0
+        #   --
+        #   3      2**1 + 1
+        #   2      2**1 + 0
+        #   --
+        #   6      2**2 + 2     | Rest of the ans in reverse
+        #   7      2**2 + 3     |
+        #   5      2**2 + 1     |
+        #   4      2**2 + 0     |
+        res = [0]
+        for i in range(n):
+            res += [ x | (1<<i) for x in res[::-1]]
+        return res
 
-        other = lambda x : '1' if x=='0' else '0'
 
-        change_bit = [0]
-        max_bit = 1
-        while int(math.log(len(change_bit)+1, 2)) < n:
-            change_bit += [max_bit] + change_bit
-            max_bit+=1
-        ans = ['{:032b}'.format(0)]
-        for c in change_bit:
-            prev = ans[-1]
-            ans.append(prev[:31-c] + other(prev[31-c]) + prev[32-c:])
-        return [int(x,2) for x in ans]
+        # =========================================
+        # Keeping track of which bit should change;
+        # Decent original solution
+        # =========================================
+        # if n == 0:
+        #     return [0]
+        #
+        # other = lambda x : '1' if x=='0' else '0'
+        #
+        # change_bit = [0]
+        # max_bit = 1
+        # while int(math.log(len(change_bit)+1, 2)) < n:
+        #     change_bit += [max_bit] + change_bit
+        #     max_bit+=1
+        # ans = ['{:032b}'.format(0)]
+        # for c in change_bit:
+        #     prev = ans[-1]
+        #     ans.append(prev[:31-c] + other(prev[31-c]) + prev[32-c:])
+        # return [int(x,2) for x in ans]
