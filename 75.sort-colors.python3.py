e4a3a76eba0164ee0ec0f7a4fd8be911e8a821db
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-05-13 17:09:59 -0700

    update 75.sort-colors.python3.py

---
+++ 75.sort-colors.python3.py
@@ -0,0 +1,66 @@
+#
+# [75] Sort Colors
+#
+# https://leetcode.com/problems/sort-colors/description/
+#
+# algorithms
+# Medium (39.08%)
+# Total Accepted:    223.1K
+# Total Submissions: 570.8K
+# Testcase Example:  '[2,0,2,1,1,0]'
+#
+# Given an array with n objects colored red, white or blue, sort them in-place
+# so that objects of the same color are adjacent, with the colors in the order
+# red, white and blue.
+#
+# Here, we will use the integers 0, 1, and 2 to represent the color red, white,
+# and blue respectively.
+#
+# Note: You are not suppose to use the library's sort function for this
+# problem.
+#
+# Example:
+#
+#
+# Input: [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+#
+# Follow up:
+#
+#
+# A rather straight forward solution is a two-pass algorithm using counting
+# sort.
+# First, iterate the array counting number of 0's, 1's, and 2's, then overwrite
+# array with total number of 0's, then 1's and followed by 2's.
+# Could you come up with a one-pass algorithm using only constant space?
+#
+#
+#
+class Solution:
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        if not any(nums):
+            return
+
+        i,li, ri = 0,0, len(nums)-1
+
+        while True:
+            while ri >= 0 and nums[ri] == 2:
+                ri-=1
+            while li < len(nums) and nums[li] == 0:
+                li+=1
+            i=max(i,li)
+            # print(nums, li, ri, i)
+            if not i <= ri:
+                break
+            if nums[i] == 2:
+                nums[i],nums[ri] = nums[ri],nums[i]
+                ri-=1
+            elif nums[i] == 0:
+                nums[i],nums[li] = nums[li],nums[i]
+                li+=1
+            else:
+                i+=1
