271658b53ed2f204c38b9c4c5feaa7e15e5e7471
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-05-19 20:50:47 -0700

    update 132.palindrome-partitioning-ii.python3.py

--- 132.palindrome-partitioning-ii.python3.py
+++ 132.palindrome-partitioning-ii.python3.py
@@ -30,19 +30,44 @@ class Solution:
         :type s: str
         :rtype: int
         """
-        def helper(st):
-            if st in table:
-                return table[st]
-            ans = float('inf')
-            if len(st) <= 1 or st == st[::-1]:
-                ans = 0
-            else:
-                n = len(st)
-                for i in range(n):
-                    head = st[:i+1]
-                    if head == head[::-1]:
-                        ans = min(ans, 1 + helper(st[i+1:]))
-            table[st] = ans
-            return ans
-        table = {}
-        return helper(s)
+        # =====================================================================
+        # Trying iterative version
+        # https://leetcode.com/problems/palindrome-partitioning-ii/discuss/42198/My-solution-does-not-need-a-table-for-palindrome-is-it-right-It-uses-only-O(n)-space./112629
+        # =====================================================================
+        n = len(s)
+        dp = [x for x in range(n)]
+
+        for i in range(n):
+            st, en = i,i
+            while st >= 0 and en < n and s[st] == s[en]:
+                dp[en] = min(dp[en], 1 + dp[st-1] if st > 0 else 0)
+                st-=1
+                en+=1
+
+            st, en = i-1,i
+            while st >= 0 and en < n and s[st] == s[en]:
+                dp[en] = min(dp[en], 1 + dp[st-1] if st > 0 else 0)
+                st-=1
+                en+=1
+
+        return dp[n-1]
+
+        # =====================================================================
+        # Recursive O(n^2) space ;; slower than iteratie version. beats only 9%
+        # =====================================================================
+        # def helper(st):
+        #     if st in table:
+        #         return table[st]
+        #     ans = float('inf')
+        #     if len(st) <= 1 or st == st[::-1]:
+        #         ans = 0
+        #     else:
+        #         n = len(st)
+        #         for i in range(n):
+        #             head = st[:i+1]
+        #             if head == head[::-1]:
+        #                 ans = min(ans, 1 + helper(st[i+1:]))
+        #     table[st] = ans
+        #     return ans
+        # table = {}
+        # return helper(s)
