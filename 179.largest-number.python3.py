b9fa4943622ef1c05013867b9c4cd97b995e3f92
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-05-21 00:54:39 -0700

    update 179.largest-number.python3.py

---
+++ 179.largest-number.python3.py
@@ -0,0 +1,54 @@
+#
+# [179] Largest Number
+#
+# https://leetcode.com/problems/largest-number/description/
+#
+# algorithms
+# Medium (23.61%)
+# Total Accepted:    94.7K
+# Total Submissions: 400.4K
+# Testcase Example:  '[10,2]'
+#
+# Given a list of non negative integers, arrange them such that they form the
+# largest number.
+#
+# Example 1:
+#
+#
+# Input: [10,2]
+# Output: "210"
+#
+# Example 2:
+#
+#
+# Input: [3,30,34,5,9]
+# Output: "9534330"
+#
+#
+# Note: The result may be very large, so you need to return a string instead of
+# an integer.
+#
+#
+class Solution:
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        if not any(nums):
+            return '0'
+        transformed = []
+        len_table = {}
+        for i,num in enumerate(nums):
+            s = str(num)
+            l = len(s)
+            len_table[i] = l
+            s += s[0]*10 + s[::-1]
+            transformed.append((s, i))
+        # print(transformed)
+        # transformed.sort(key = lambda args : len_table[args[1]])
+        # print(transformed)
+        transformed.sort(reverse=True, key = lambda args: args[0])
+        # print(transformed)
+        ans_list = [s[:len_table[idx]] for (s,idx) in transformed]
+        return ''.join(ans_list)
