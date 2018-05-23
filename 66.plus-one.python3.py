4b3e2a702393a3c424fe11f23b7f1bd82a36054c
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-05-22 20:51:40 -0700

    update 66.plus-one.python3.py

---
+++ 66.plus-one.python3.py
@@ -0,0 +1,51 @@
+#
+# [66] Plus One
+#
+# https://leetcode.com/problems/plus-one/description/
+#
+# algorithms
+# Easy (39.84%)
+# Total Accepted:    247.4K
+# Total Submissions: 621K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a non-empty array of digits representing a non-negative integer, plus
+# one to the integer.
+#
+# The digits are stored such that the most significant digit is at the head of
+# the list, and each element in the array contain a single digit.
+#
+# You may assume the integer does not contain any leading zero, except the
+# number 0 itself.
+#
+# Example 1:
+#
+#
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+#
+#
+# Example 2:
+#
+#
+# Input: [4,3,2,1]
+# Output: [4,3,2,2]
+# Explanation: The array represents the integer 4321.
+#
+#
+#
+class Solution:
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        carry = 1
+        for i in range(len(digits)-1, -1, -1):
+            s = digits[i] + carry
+            digits[i] = s%10
+            carry = s//10
+        if carry>0:
+            digits.insert(0, carry)
+        return digits
