3af9e68aebde2a54eefc5f60a2a7f0a8b32deb1f
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-06-02 12:37:02 -0700

    update 686.repeated-string-match.python3.py

---
+++ 686.repeated-string-match.python3.py
@@ -0,0 +1,45 @@
+#
+# [686] Repeated String Match
+#
+# https://leetcode.com/problems/repeated-string-match/description/
+#
+# algorithms
+# Easy (31.81%)
+# Total Accepted:    31K
+# Total Submissions: 97.7K
+# Testcase Example:  '"abcd"\n"cdabcdab"'
+#
+# Given two strings A and B, find the minimum number of times A has to be
+# repeated such that B is a substring of it. If no such solution, return -1.
+#
+#
+# For example, with A = "abcd" and B = "cdabcdab".
+#
+#
+# Return 3, because by repeating A three times (“abcdabcdabcd”), B is a
+# substring of it; and B is not a substring of A repeated two times
+# ("abcdabcd").
+#
+#
+# Note:
+# The length of A and B will be between 1 and 10000.
+#
+#
+class Solution:
+    def repeatedStringMatch(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: int
+        """
+        n = 1
+        nA = A
+        while len(nA) < len(B):
+            nA += A
+            n+=1
+        if B in nA:
+            return n
+        elif B in nA + A:
+            return n+1
+        else:
+            return -1
