f8796b60c080c9918f8458f464e288696201283d
ckidckidckid <38821370+ckidckidckid@users.noreply.github.com>
2018-06-29 15:02:29 -0700

    update 342.power-of-four.python3.py

---
+++ 342.power-of-four.python3.py
@@ -0,0 +1,33 @@
+#
+# [342] Power of Four
+#
+# https://leetcode.com/problems/power-of-four/description/
+#
+# algorithms
+# Easy (39.28%)
+# Total Accepted:    89K
+# Total Submissions: 226.1K
+# Testcase Example:  '16'
+#
+#
+# Given an integer (signed 32 bits), write a function to check whether it is a
+# power of 4.
+#
+# Example:
+# Given num = 16, return true.
+# Given num = 5, return false.
+#
+#
+# Follow up: Could you solve it without loops/recursion?
+#
+# Credits:Special thanks to @yukuairoy  for adding this problem and creating
+# all test cases.
+#
+class Solution:
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        s = bin(num)[2:]
+        return num >= 0 and s.count('0')%2 == 0 and s.count('1') == 1
